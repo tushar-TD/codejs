85b89f4a99531912c21bcb76154a03be5534128c
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-11-27 09:59:52 +0000

    var/let/const

---
+++ 01_basics/var.js
@@ -0,0 +1,14 @@
+const accountId=144553;
+let accounEmail="tushardhanorkar786@gmail.com"
+var accounPass="123pass"
+accountCity="jaipur"
+
+
+//accountId=90; not allow to modify--const
+accounEmail="sc@gmail.com"
+accounPass=9090;
+accountCity="mumbai"
+/*prefer not to use var---> cos issue in block and functional scope 
+*/
+console.log(accountId)
+console.table([accounEmail,accounPass,accountCity])
