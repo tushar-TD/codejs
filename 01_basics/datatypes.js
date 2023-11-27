2153bd6216e663a9ca5a297bf0d1ff378c4d454f
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-11-27 10:15:33 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+//alert(90+10) we are using nodejs,not browser
+
+let naame="Tushar"
+let age=23;
+//number=>2^53
+//bigint=>
+//string=>
+//boolean=>
+//null=>standalone value
+//undefined=>not assign value
+//symbol=>to find components in react for uniqueness
+
+//object
+
+console.log(typeof(null))//null is object and undefined is undefined
