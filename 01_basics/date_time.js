6f5a47788bd01de13061393ca1b8c6374ecc37ed
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-11-29 09:59:15 +0000

    date

---
+++ 01_basics/date_time.js
@@ -0,0 +1,15 @@
+let mydate=new Date()
+console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleTimeString())
+// console.log(typeof(mydate))
+// console.log()
+
+// let mydat=new Date(2023,0,23)
+// console.log(mydat.getMonth())
+
+// let mytimeStamps=Date.now()
+// console.log(mydat.getDay())
+
