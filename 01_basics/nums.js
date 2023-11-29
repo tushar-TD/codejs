75308a0df8ac5b8d331169c3ec8b6d2f6f807f29
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-11-29 09:23:32 +0000

    Math methods

---
+++ 01_basics/nums.js
@@ -0,0 +1,22 @@
+// const score=900;
+// console.log(score)
+
+// const balance=new Number(900)
+// console.log(balance.toString())
+// console.log(typeof(balance))
+
+// const num=908989333
+// console.log(num.toLocaleString('en-IN'))
+
+//Maths library
+// console.log(Math)
+// console.log(Math.round(-67.90))
+// console.log(Math.floor(-67.90))
+// console.log(Math.ceil(-67.90))
+// console.log(Math.abs(-67.90))
+// console.log(Math.random()*10)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
