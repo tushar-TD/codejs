15d3bc599d2150836a735e274ca5be40080237b8
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-11-30 12:04:11 +0000

    destructing

---
+++ 02_basics/obj3.js
@@ -0,0 +1,9 @@
+//destructuring of object
+const course={
+    courseName:"Js by Tushar",
+    coursePrice:1000,
+    courseInstructor:"Tushar Dhanorkar"
+}
+const {courseInstructor:ci}=course//destrcuting of obj
+console.log(ci)
+
