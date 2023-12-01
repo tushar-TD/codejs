c5cab66413088b93b73f75252649004a018dac96
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-12-01 19:01:58 +0000

    scopes

---
+++ 02_basics/scope.js
@@ -0,0 +1,25 @@
+
+// var b=200;
+// let a =70;
+
+// if(true){
+//     let a=10;
+//     var b=20;
+//     const c=90;
+//     console.log(a)
+// }
+// //console.log(a)
+// console.log(b)
+// //console.log(c)
+
+function one(){
+    const userName="Tushar"
+
+    function two(){
+    const website="Google"
+    console.log(userName)
+    }
+    //console.log(website)
+    two()
+}
+one()
