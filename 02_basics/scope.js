9dad21fd6c95fc12785b49fb2cc56dfca17e6697
tushar-TD <136827623+tushar-TD@users.noreply.github.com>
2023-12-01 19:09:36 +0000

    add

--- 02_basics/scope.js
+++ 02_basics/scope.js
@@ -1,25 +1,37 @@
 
-// var b=200;
-// let a =70;
+// // var b=200;
+// // let a =70;
 
-// if(true){
-//     let a=10;
-//     var b=20;
-//     const c=90;
-//     console.log(a)
+// // if(true){
+// //     let a=10;
+// //     var b=20;
+// //     const c=90;
+// //     console.log(a)
+// // }
+// // //console.log(a)
+// // console.log(b)
+// // //console.log(c)
+
+// function one(){
+//     const userName="Tushar"
+
+//     function two(){
+//     const website="Google"
+//     console.log(userName)
+//     }
+//     //console.log(website)
+//     two()
 // }
-// //console.log(a)
-// console.log(b)
-// //console.log(c)
+// one()
 
-function one(){
-    const userName="Tushar"
-
-    function two(){
-    const website="Google"
-    console.log(userName)
-    }
-    //console.log(website)
-    two()
+console.log(addit(5))
+const n=function addit(num){
+return num+1;
 }
-one()
+
+
+function addtwo (num1){
+return num1+2;
+}
+//addtwo(5);
+
